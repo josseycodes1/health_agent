7144bca265c89da30b7fbf3721c9c98c2517903d
josseycodes1 <adewumijosephine1@gmail.com>
2025-11-02 23:04:09 +0100

    debug

--- health_tips/urls.py
+++ health_tips/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import A2AHealthView, HealthCheckView
 
 urlpatterns = [
-    path('a2a/health/', A2AHealthView.as_view(), name='a2a_health'),
-    path('health/', HealthCheckView.as_view(), name='health_check'),
+    path('a2a/health', A2AHealthView.as_view(), name='a2a_health'),
+    path('health', HealthCheckView.as_view(), name='health_check'),
 ]
