9a23475ac30807a55baeb4dd92c6d9e21800d66b
josseycodes1 <adewumijosephine1@gmail.com>
2025-11-02 23:00:24 +0100

    new url

--- health_agent/urls.py
+++ health_agent/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('health_tips.urls')),
+    path('', include('health_tips.urls')),
 ]
